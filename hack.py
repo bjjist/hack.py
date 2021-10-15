a489dd2820d93440bd5062c87145a1e00c1af2fa
bjjist <tukanbro5@gmail.com>
2021-10-15 21:02:34 +0300

    hatsker

---
+++ hack.py
@@ -0,0 +1,10 @@
+
+import time 
+prosent = 0
+while True:
+    if  prosent < 100:
+        prosent= prosent + 10
+        time.sleep(2)
+        print("ataka pentagona zavershena na" + str(prosent) )
+
+
